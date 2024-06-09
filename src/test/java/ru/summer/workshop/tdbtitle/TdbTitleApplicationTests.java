2d1b063dcb214b4a16df2d85f1e38c44269febba
fyodorstruzhanov <fedorstrujanov26@gmail.com>
2024-06-09 13:09:06 +0300

    - инициализировал проект - добавил базовые спринг зависимости

---
+++ src/test/java/ru/summer/workshop/tdbtitle/TdbTitleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.summer.workshop.tdbtitle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdbTitleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
