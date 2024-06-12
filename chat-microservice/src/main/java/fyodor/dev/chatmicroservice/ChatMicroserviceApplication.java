e0f096cc7746498ae518ae9ee74986eba659c262
fyodorstruzhanov <fedorstrujanov26@gmail.com>
2024-06-12 18:33:00 +0300

    - добавил конфиги для микросервисов - добавил тестовые контроллеры

--- chat-microservice/src/main/java/fyodor/dev/chatmicroservice/ChatMicroserviceApplication.java
+++ chat-microservice/src/main/java/fyodor/dev/chatmicroservice/ChatMicroserviceApplication.java
@@ -2,8 +2,10 @@ package fyodor.dev.chatmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ChatMicroserviceApplication {
 
     public static void main(String[] args) {
