8a246c8b2083765c3bf017b6ac6a11d167e9af75
fyodorstruzhanov <fedorstrujanov26@gmail.com>
2024-06-23 22:40:45 +0300

    - настройки роутинга

--- api-gateway/src/main/java/fyodor/dev/apigateway/filter/RouteValidator.java
+++ api-gateway/src/main/java/fyodor/dev/apigateway/filter/RouteValidator.java
@@ -13,7 +13,7 @@ public class RouteValidator {
             "/auth/register",
             "/auth/login",
             "/auth/refresh",
-            "core/**",
+//            "core/**",
             "/eureka"
     );
 
