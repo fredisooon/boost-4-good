ea6dfecfc6af2430ba102a546680e618037a7d4d
fyodorstruzhanov <fedorstrujanov26@gmail.com>
2024-06-29 18:51:41 +0300

    - убрал неиспользуемые конфиги для старта микросервисов

--- chat-microservice/src/main/java/fyodor/dev/chatmicroservice/ChatMicroserviceApplication.java
+++ chat-microservice/src/main/java/fyodor/dev/chatmicroservice/ChatMicroserviceApplication.java
@@ -2,10 +2,15 @@ package fyodor.dev.chatmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        JpaRepositoriesAutoConfiguration.class,
+})
 public class ChatMicroserviceApplication {
 
     public static void main(String[] args) {
--- eureka-server/src/main/java/fyodor/dev/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/fyodor/dev/eurekaserver/EurekaServerApplication.java
@@ -2,10 +2,15 @@ package fyodor.dev.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		JpaRepositoriesAutoConfiguration.class,
+})
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
